48a2ec557b548e637f9d5459ce9b533ecfaeb840
franco sassi <franco.sassi97@gmail.com>
2022-12-09 00:26:06 -0300

    mejora entrega

--- js/main.js
+++ js/main.js
@@ -2,12 +2,27 @@ let nombre = prompt("Bienvenido al Bar, ingresa tu nombre!")
 
 alert("Bienvenido/a" + " " + nombre)
 
-let pizza = 2500;
-let panino = 1900;
-let empanada = 280;
-let ravioles = 950;
-let carlitos = 1790;
+const pizza = 2500;
+const pizza2 = 3000;
+
+const panino = 1900;
+const panino2 = 2000;
+
+const empanada = 280;
+const empanada2 = 300;
+
+const ravioles = 950;
+const ravioles2 = 1000;
+
+const carlitos = 1790;
+const carlitos2 = 2000;
+
 let totalF = 0;
+
+function error(){
+    alert("Opcion incorrecta, seleccione opcion valida o ESC para finalizar.");
+}
+
 
 let comidas = prompt("Elige una comida de nuestro menu: \npizza \nempanada \npanino \ncarlitos \nravioles \nPresiona ESC para finalizar.")
 while (comidas != "ESC") {
@@ -17,65 +32,55 @@ while (comidas != "ESC") {
             let tipo = prompt("Elija de mozarella o de fugazza");
             if (tipo === "mozarella") {
                 totalF = parseInt(totalF + pizza)
-                alert("El total de tu compra es de" + " " + "$" + totalF);
             } else if (tipo === "fugazza") {
-                totalF = parseInt(totalF + pizza)
-                alert("El total de tu compra es de" + " " + "$" + totalF);
+                totalF = parseInt(totalF + pizza2)
             }
             else {
-                alert("Opcion incorrecta, seleccione opcion valida o ESC para finalizar.");
+                error();
             } 
             break;
         case "panino":
             let tipo2 = prompt("Elija entre familiar o vegetariano");
             if (tipo2 === "familiar") {
-                totalF = parseInt(totalF + empanada)
-                alert("El total de tu compra es de" + " " + "$" + totalF);
+                totalF = parseInt(totalF + panino)
             } else if (tipo2 === "vegetariano") {
-                totalF = parseInt(totalF + empanada)
-                alert("El total de tu compra es de" + " " + "$" + totalF);
+                totalF = parseInt(totalF + panino2)
             }
             else {
-                alert("Opcion incorrecta, seleccione opcion valida o ESC para finalizar.");
+                error();
             }
             break;
         case "empanada":
             let tipo3 = prompt("Elija entre capresse o de verdura");
             if (tipo3 === "capresse") {
-                totalF = parseInt(totalF + panino)
-                alert("El total de tu compra es de" + " " + "$" + totalF);
+                totalF = parseInt(totalF + empanada)
             } else if (tipo3 === "verdura") {
-                totalF = parseInt(totalF + panino)
-                alert("El total de tu compra es de" + " " + "$" + totalF);
+                totalF = parseInt(totalF + empanada2)
             }
             else {
-                alert("Opcion incorrecta, seleccione opcion valida o ESC para finalizar.");
+                error();
             }
             break;
         case "ravioles":
             let tipo4 = prompt("Elija entre de carne o de ricota");
             if (tipo4 === "carne") {
                 totalF = parseInt(totalF + ravioles)
-                alert("El total de tu compra es de" + " " + "$" + totalF);
             } else if (tipo4 === "ricota") {
-                totalF = parseInt(totalF + ravioles)
-                alert("El total de tu compra es de" + " " + "$" + totalF);
+                totalF = parseInt(totalF + ravioles2)
             }
             else {
-                alert("Opcion incorrecta, seleccione opcion valida o ESC para finalizar.");
+                error();
             }
             break;
         case "carlitos":
             let tipo5 = prompt("Elija entre simple o de pollo");
             if (tipo5 === "simple") {
                 totalF = parseInt(totalF + carlitos)
-                alert("El total de tu compra es de" + " " + "$" + totalF);
             } else if (tipo5 === "pollo") {
-                totalF = parseInt(totalF + carlitos)
-                alert("El total de tu compra es de" + " " + "$" + totalF);
+                totalF = parseInt(totalF + carlitos2)
             }
             else {
-                alert("Opcion incorrecta, seleccione opcion valida o ESC para finalizar.");
+                error();
             }
             break;
 
@@ -86,4 +91,5 @@ while (comidas != "ESC") {
     comidas= prompt("Elige una comida de nuestro menu: \npizza \nempanada \npanino \ncarlitos \nravioles \nPresiona ESC para finalizar.")
 
 
-}
+} 
+alert ("el total a pagar es "+totalF)
